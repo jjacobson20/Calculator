4d14407d19bac794887cdfdd59ffabad9e77919a
jjacobson20 <xxk_@outlook.com>
2018-09-20 01:15:14 -0400

    add files

---
+++ src/CalcApp.java
@@ -0,0 +1,11 @@
+public class CalcApp
+{
+	public static void main(String[] args)
+	{
+		Calculator thisCalculator = new Calculator();
+		int minuend = 10;
+		int subtrahend = 6;
+		int diff = thisCalculator.subtract(10,6);
+		System.out.println("The difference of " + minuend + " and " + subtrahend + " is: " + diff);
+	}
+}
